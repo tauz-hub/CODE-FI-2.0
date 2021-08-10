d5ad14eda52128128ce13c6e6e5a30b0e8496cb6
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-08-09 23:11:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ client.on('ready', async() => {
 
 setInterval(async function() {
     if (!client.voice.connections.size) {
-
+  console.log("desconectado")
         if (!channel) return;
         try {
             const connection = await channel.join();
@@ -86,6 +86,6 @@ setInterval(async function() {
             console.error(error);
         }
     }
-}, 2000);
+}, 500);
 
 client.login(token);
